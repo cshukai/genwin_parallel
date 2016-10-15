198ca84021d63f2487f0fdf2eeb520bb2e845ab3
sc724 <kz724@outlook.com>
2016-10-15 17:20:32 +0000

    sort by snp location

--- wrapper.r
+++ wrapper.r
@@ -1,10 +1,25 @@
-# combining idv files
+#####################data preporcessing#####################
+#combining idv files
 files=Sys.glob(file.path( "*.out"))
-
+pos_col_idx=3
 d=NULL
 for(i in 1:length(files)){
     thisfile=read.table(files[i],sep="\t",skip=1)
-    d=rbind(d,thisfile)
+    thisfile2=thisfile[order(thisfile[,pos_col_idx]),]#sort table on snp location 
+    d=rbind(d,thisfile2)
+}
+
+
+
+
+
+#########################HPC############################
+splineAnalyze.idvChrom<-function(d,hpc_mode){
+    
+}
+
+splineAnalyze.multiChrom<-function(d,hpc_mode){
+    
 }
 #sort by chromosome
 #sort by snp position
