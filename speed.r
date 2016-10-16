85454f35a7653e8d09c56907cbf758145e3f61c6
sc724 <kz724@outlook.com>
2016-10-16 22:22:35 +0000

    lindberg for loop benchmark multi chr done

--- speed.r
+++ speed.r
@@ -1,13 +1,10 @@
-##########################test 1#######################
+##########################test for single chromosome#######################
 tic()
 chr6Spline <- splineAnalyze(Y=chr6$Fst,map=chr6$Position,smoothness=100,plotRaw=TRUE,plotWindows=TRUE,method=4)
 toc()
-#170.4 sec elapsed  8g laptop
-#49.945 sec elapsed 32g server datascience-2
-# 33.042 sec elapsed 46g  server lindberg
 
 
-######################test2################################
+
 threads= 12
 cl=makeCluster(threads)
 registerDoSNOW(cl)
@@ -16,4 +13,48 @@ tic()
 chr6Spline2 <- splineAnalyze2(Y=chr6$Fst,map=chr6$Position,smoothness=100,plotRaw=TRUE,plotWindows=TRUE,method=4)
 toc()
 stopCluster(cl)
-##
+
+
+# without optimization
+    #170.4 sec elapsed  8g laptop
+    #49.945 sec elapsed 32g server datascience-2
+    # 33.042 sec elapsed 46g  server lindberg
+
+# for loop optimization : 10.403 sec elapsed
+
+
+####################test for multiple chromosome##################
+#combining idv files
+files=Sys.glob(file.path( "*.out"))
+pos_col_idx=3
+d=list()
+for(i in 1:length(files)){
+    thisfile=read.table(files[i],sep="\t",skip=1)
+    thisfile2=thisfile[order(thisfile[,pos_col_idx]),]#sort table on snp location 
+    d[[i]]=thisfile2
+}
+
+
+
+allChrSplines=list()
+
+# without optimization 
+tic()
+for(i in 1:length(d)){
+    thisfile=d[[i]]
+    allChrSplines[[i]]= splineAnalyze(Y=thisfile[,4],map=thisfile[,3],smoothness=100,plotRaw=TRUE,plotWindows=TRUE,method=4)
+}
+toc()
+#66.305 sec elapsed on lindberg
+# with optimization
+
+threads= 12
+cl=makeCluster(threads)
+registerDoSNOW(cl)
+
+tic()
+allChrSplines2=list()
+foreach(k=1:length(length(d)) %dopar% 
+chr6Spline2 <- splineAnalyze2(Y=chr6$Fst,map=chr6$Position,smoothness=100,plotRaw=TRUE,plotWindows=TRUE,method=4)
+toc()
+stopCluster(cl)
--- wrapper.r
+++ wrapper.r
@@ -4,29 +4,4 @@ cl=makeCluster(threads)
 registerDoSNOW(cl)
 
 
-#####################data preporcessing#####################
-#combining idv files
-files=Sys.glob(file.path( "*.out"))
-pos_col_idx=3
-d=NULL
-for(i in 1:length(files)){
-    thisfile=read.table(files[i],sep="\t",skip=1)
-    thisfile2=thisfile[order(thisfile[,pos_col_idx]),]#sort table on snp location 
-    d=rbind(d,thisfile2)
-}
-
-
-
-
-
-#########################HPC############################
-splineAnalyze.idvChrom<-function(d,hpc_mode){
-    
-}
-
-splineAnalyze.multiChrom<-function(d,hpc_mode){
-    
-}
-#sort by chromosome
-#sort by snp position
 
