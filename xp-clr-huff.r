0ddf25cacb31a54a77bfd085071e5c0470a75b46
sc724 <kz724@outlook.com>
2016-11-10 23:35:26 +0000

    remove null

--- xp-clr-huff.r
+++ xp-clr-huff.r
@@ -1,9 +1,17 @@
-#####################input & data preprocessing##################
-d=read.table("Hufford_et_al._2012_10kb_statistics.txt",sep="\t",header=T)
+#################useful fields##########################
 #chr: AGPv1 chromosome
 #XPCLR_DOM: XP-CLR statistic for selection during domestication (Chen etal. 2010)
 #XPCLR_IMP: XP-CLR statistic for selection during improvement (Chen et al. 2010)
 #winstart: AGPv1 position for window start site
 #winend: AGPv1 position for window end site
 #genicbp: number of genic base pairs in window
+
+#####################input & data preprocessing##################
+d=read.table("Hufford_et_al._2012_10kb_statistics.txt",sep="\t",header=T)
+d_dom=d[which(!is.na(d[,"XPCLR_DOM"])),]
+d_imp=d[which(!is.na(d[,"XPCLR_IMP"])),]
+
 ######################fit genwin ######################
+
+
+save.image("xpclr.RData")
