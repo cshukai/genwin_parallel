a575dade0dd8495a11e44c984a6c8690a4d3e57b
sc724 <kz724@outlook.com>
2016-11-12 21:04:48 +0000

    split by chromosome

--- xp-clr-huff.r
+++ xp-clr-huff.r
@@ -7,11 +7,25 @@
 #genicbp: number of genic base pairs in window
 
 #####################input & data preprocessing##################
+#read data
 d=read.table("Hufford_et_al._2012_10kb_statistics.txt",sep="\t",header=T)
 d_dom=d[which(!is.na(d[,"XPCLR_DOM"])),]
 d_imp=d[which(!is.na(d[,"XPCLR_IMP"])),]
+#sort for later spline fit
+d_dom_sorted=d_dom[order(d_dom$winstart),]
+d_imp_sorted=d_imp[order(d_imp$winstart),]
+#split by chromosome
+chrNum=10
+chr_spec_dom=list()
+chr_spec_imp=list()
+for(i in 1:chrNum){
+    chr_spec_dom[[i]]=d_dom_sorted[which(d_dom_sorted[,"chr"]==i),]
+    chr_spec_imp[[i]]=d_imp_sorted[which(d_imp_sorted[,"chr"]==i),]
+    
+}
+
 
 ######################fit genwin ######################
-
+domSpline=splineAnalyze(Y=d_dom_sorted$XPCLR_DOM,map=mean(d_dom_sorted$winstart+d_dom_sorted$winend),smoothness=100,plotRaw=TRUE,plotWindows=TRUE,method=4)
 
 save.image("xpclr.RData")
