6743b3a6c93d072ba096cf336fc73f08f3bb2d67
sc724 <kz724@outlook.com>
2017-12-04 18:12:57 +0000

    data cleaning

--- recombination_intepolation.r
+++ recombination_intepolation.r
@@ -1,2 +1,19 @@
 rec=read.table("GeneticMapFromNam.txt",header=T)
 fst=read.table("FstTable.txt",header=T)
+
+
+#clean out missing data and correct misplaced start/end  
+colnames(rec)=c("p.rs.","chr","start","end","cM")
+rec2=NULL
+for(i in 1:nrow(rec)){
+  if(!is.na(rec[i,"start"]) && !is.na(rec[i,"end"]) ){
+     if(rec[i,"start"]<rec[i,"end"]){
+       thisRow=c(rec[i,"p.rs."],rec[i,"chr"],rec[i,"start"],rec[i,"end"],rec[i,"cM"])
+       rec2=rbind(rec2,thisRow)
+     }
+     else{
+         thisRow=c(rec[i,"p.rs."],rec[i,"chr"],rec[i,"end"],rec[i,"start"],rec[i,"cM"])
+         rec2=rbind(rec2,thisRow)
+     }
+  }
+}
